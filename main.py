f91c96a4b7fe58482dcdedef9fac0bc431ad852a
Joseph <joseph.doe@ctful.ca>
2023-03-15 17:31:59 -0400

    Ajout de commentaires

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import random
 
+# Génération d'un nombre entre 0 et 1000 inclus
 number = random.randint(0, 1000)
 found = False
 
 print("Vous devez trouver un nombre aléatoire entre 0 et 1000")
 
+# Tant que le nombre n'est pas trouvé
 while not found:
     print("Entez un nombre : ")
+    # On demande à l'utilisateur d'entrer un nomnbre
     x = int(input())
 
     if x == number:
